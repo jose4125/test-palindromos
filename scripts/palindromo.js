6ddbc07cfe91cdd67aac8db1b823427457660698
jd <jose4125@gmail.com>
2015-03-29 21:23:01 -0500

    refactor constructor - lowercase and remove special characters methods

--- scripts/palindromo.js
+++ scripts/palindromo.js
@@ -2,20 +2,28 @@
 var Palindrome = (function () {
   var Palindrome = {
     constructor: function (message) {
-      this.message = message.toLowerCase().replace(/[\\.&+%#*?\[^\]$()@{}=!<>|:,-\s]/g, '');
+      this.message = message;
       this.arrayCheck = [];
+      this.message = this.lowerCase(this.message);
+      this.message = this.removeSpecialCharacters(this.message);
+    },
+    lowerCase: function (message) {
+      return message.toLowerCase();
+    },
+    removeSpecialCharacters: function (message) {
+      return message.replace(/[\\.&+%#*?\[^\]$()@{}=!<>|:,-\s]/g, '');
     },
     isPalindrome: function () {
       if (!this.message) {
         return true;
       }
       dump(this.message);
-      if(this.message.length === 1) {
+      if (this.message.length === 1) {
         return true;
-      }else {
+      } else {
         for (var i = 0; i < Math.floor((this.message.length / 2)); i++) {
-          dump(i, this.message[i], this.message[this.message.length - (i+1)]);
-          if(this.message[i] !== this.message[this.message.length - (i+1)]){
+          dump(i, this.message[i], this.message[this.message.length - (i + 1)]);
+          if (this.message[i] !== this.message[this.message.length - (i + 1)]) {
             return false;
           }
         }
