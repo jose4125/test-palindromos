6410769cb4bb63087dc322258570d1c5a3da1408
jd <jose4125@gmail.com>
2015-04-03 20:58:24 -0500

    change t modulo revelado pattern

--- scripts/palindromo.js
+++ scripts/palindromo.js
@@ -1,57 +1,58 @@
 'use strict';
 var Palindrome = (function () {
-  var Palindrome = {
-    constructor: function (message) {
-      this.message = message;
-      this.arrayCheck = [];
-      this.message = this.lowerCase(this.message);
-      this.message = this.removeSpecialCharacters(this.message);
-    },
-    lowerCase: function (message) {
-      return message.toLowerCase();
-    },
-    removeSpecialCharacters: function (message) {
-      return message.replace(/[\\.&+%#*?\[^\]$()@{}=!<>|:,-\s]/g, '');
-    },
-    messageLength: function () {
-      return this.message.length;
-    },
-    middleMessageLength: function () {
-      return Math.floor((this.messageLength() / 2));
-    },
-    getLeftCharacter: function (index) {
-      return this.message[index];
-    },
-    getRightCharacter: function (index) {
-      return this.message[this.messageLength() - (index + 1)];
-    },
-    verifyEnds: function (index) {
-      if (this.getLeftCharacter(index) !== this.getRightCharacter(index)) {
-        return false;
-      }
-      return true;
-    },
-    aechEnds: function () {
-      var isEqual;
-      for (var i = 0; i < this.middleMessageLength(); i++) {
-        //dump(i, this.message[i], this.message[this.message.length - (i + 1)]);
-        isEqual = this.verifyEnds(i);
-      }
-      dump('palindrome ' + isEqual);
-      return isEqual;
-    },
-    isPalindrome: function () {
-      if (!this.message) {
-        return true;
-      }
-      dump(this.message);
-      if (this.messageLength() === 1) {
-        return true;
-      } else {
-        return this.aechEnds();
-      }
+  var _message = '';
+  //var arrayCheck = [];
+  function constructor(text) {
+    _message = text;
+    _message = _lowerCase(_message);
+    _message = _removeSpecialCharacters(_message);
+  }
+  function _lowerCase(message) {
+    return message.toLowerCase();
+  }
+  function _removeSpecialCharacters(message) {
+    return message.replace(/[\\.&+%#*?\[^\]$()@{}=!<>|:,-\s]/g, '');
+  }
+  function _messageLength() {
+    return _message.length;
+  }
+  function _middleMessageLength() {
+    return Math.floor((_messageLength() / 2));
+  }
+  function _getLeftCharacter(index) {
+    return _message[index];
+  }
+  function _getRightCharacter(index) {
+    return _message[_messageLength() - (index + 1)];
+  }
+  function _verifyEnds(index) {
+    if (_getLeftCharacter(index) !== _getRightCharacter(index)) {
+      return false;
     }
-  };
+    return true;
+  }
+  function _aechEnds() {
+    var isEqual;
+    for (var i = 0; i < _middleMessageLength(); i++) {
+      //dump(i, message[i], message[message.length - (i + 1)]);
+      isEqual = _verifyEnds(i);
+    }
+    dump('palindrome ' + isEqual);
+    return isEqual;
+  }
+  function isPalindrome() {
+    if (!_message) {
+      return true;
+    }
+    if (_messageLength() === 1) {
+      return true;
+    } else {
+      return _aechEnds();
+    }
+  }
 
-  return Palindrome;
+  return {
+    constructor: constructor,
+    isPalindrome: isPalindrome
+  };
 })();
